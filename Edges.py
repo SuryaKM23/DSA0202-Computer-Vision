b85fd0ffe0412a7d560071b8dc50485502093a3b
Surya K M <115336797+SuryaKM23@users.noreply.github.com>
2023-05-15 13:07:22 +0530

    Add files via upload

---
+++ Edges.py
@@ -0,0 +1,8 @@
+#Edge detection using canny method
+import cv2
+import numpy as np
+img = cv2.imread("C:/Users/surya/PycharmProjects/pythonProject/thalapathy.jpg")
+edges = cv2.Canny(img, 100, 200)
+cv2.imshow("Original", img)
+cv2.imshow("Edges", edges)
+cv2.waitKey(0)
